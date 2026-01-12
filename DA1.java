578f2e95a9f3a19dc1204d2989633d77c005c89a
NARMATHA N <narmathan@NARMATHAs-MacBook-Air.local>
2026-01-12 14:39:07 +0530

    Fixed Java syntax error

--- DA1.java
+++ DA1.java
@@ -1,9 +1,9 @@
-System.out.println("Message: Jenkins-GitHub Integration Successful");
 public class DA1 {
     public static void main(String[] args) {
         System.out.println("Student Name: Narmatha N");
         System.out.println("Register Number: 23MIC0049");
-        System.out.println("Message: Jenkins–GitHub Integration Successful");
+        System.out.println("Message: Jenkins-GitHub Integration Successful");
     }
 }
+
 
