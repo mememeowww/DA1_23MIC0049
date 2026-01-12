a396b83b144566f8a4cb134be09231fa4b6ae1b8
NARMATHA N <narmathan@NARMATHAs-MacBook-Air.local>
2026-01-12 14:19:13 +0530

    Initial commit - Java program for DA1

---
+++ DA1.java
@@ -0,0 +1,8 @@
+public class DA1 {
+    public static void main(String[] args) {
+        System.out.println("Student Name: Narmatha N");
+        System.out.println("Register Number: 23MIC0049");
+        System.out.println("Message: Jenkins–GitHub Integration Successful");
+    }
+}
+
